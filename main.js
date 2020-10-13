43f7ba4132c37099e869489d3ee5f70428f464e8
Abhigyan2010 <72396135+Abhigyan2010@users.noreply.github.com>
2020-10-13 10:14:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function P1(){
+    var input1=[];
+     for(var i=0; i<=6; i++){
+         input1.push(document.getElementById("para1_input_box"+i).value);
+         input1.join(". ");
+         document.getElementById("S1P").innerHTML=input1.join(". ");
+     }
+}
+function P2(){
+    var input2=[];
+     for(var j=0; j<=6; j++){
+         input2.push(document.getElementById("para2_input_box"+j).value);
+         input2.join(". ");
+         document.getElementById("S2P").innerHTML=input2.join(". ");
+     }
+}
