f29484c0cccc40b5243ef97de5d6a2d17f6f8a17
Abhigyan Prakash <72396135+Abhigyan2010@users.noreply.github.com>
2020-10-30 19:18:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function P1(){
     var input1=[];
-     for(var i=0; i<=6; i++){
+     for(var i=1; i<=6; i++){
          input1.push(document.getElementById("para1_input_box"+i).value);
          input1.join(". ");
          document.getElementById("S1P").innerHTML=input1.join(". ");
@@ -8,7 +8,7 @@ function P1(){
 }
 function P2(){
     var input2=[];
-     for(var j=0; j<=6; j++){
+     for(var j=1; j<=6; j++){
          input2.push(document.getElementById("para2_input_box"+j).value);
          input2.join(". ");
          document.getElementById("S2P").innerHTML=input2.join(". ");
